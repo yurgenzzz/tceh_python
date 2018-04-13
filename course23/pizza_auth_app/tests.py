b6b4e08f3eee11bcd9d2d718f2977ca18adc0706
golem131 <golem131@gmail.com>
2018-04-13 16:14:56 +0300

    Lecture #23

--- course23/pizza_auth_app/tests.py
+++ course23/pizza_auth_app/tests.py
@@ -40,30 +40,6 @@ class TestLoginView(TestCase):
                              errors='Please enter a correct username and password. Note that both fields may be case-sensitive.')
         self.assertEqual(Session.objects.all().count(), 0)
 
-    def test_correct_login(self):
-        with self.assertTemplateUsed(template_name='auth_app/login.html'):
-            url = reverse('auth_app:login')
-            response = self.client.post(url, {
-                'username': self.user.username,
-                'password': 'test',
-            }, follow=True)
-
-
-        self.assertEqual(Session.objects.all().count(), 1)
-
-    # def test_correct_login(self):
-    #     with self.assertTemplateUsed(template_name='auth_app/login.html'):
-    #         url = reverse('auth_app:login')
-    #         response = self.client.post(url, {
-    #             'username':self.user.username,
-    #             'password': self.user.password
-    #         }, follow=True)
-    #
-    #     self.assertTemplateUsed(response, template_name='pizza_app/_base.html')
-    #     # self.assertEqual(self.user.is_authenticated, True)
-    #     self.assertEqual(Session.objects.all().count(), 1)
-    #     self.assertEqual(self.user.is_authenticated, True)
-
 
 # TODO: add tests for correct login
 # TODO: add tests for registration
